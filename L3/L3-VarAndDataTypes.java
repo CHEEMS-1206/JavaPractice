b53915eb935cfed89a92302f1fcc7eb41e10ac3e
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-10-10 15:34:16 +0530

    Variables and data types in java

---
+++ L3/L3-VarAndDataTypes.java
@@ -0,0 +1,40 @@
+class Variables {
+    public static void main(String a[]){
+
+        System.out.println("All Data Types --> ");
+
+        // Integers
+        byte byt = 2;
+        short sh = 23;
+        int in = 567;
+        long lng = 456l; // use l as suffix 
+
+        // Floats
+        float flt = 5.23f; // use f as suffix
+        double dbl = 45.568;
+
+        // Characters
+        char ch = 't'; // string in "" and char in ''
+
+        // Booleans
+        boolean bl = true; // either true or false, true != 1/0 and false != 1/0 
+    }
+}
+
+// Java is a strongly typed language 
+
+// for creating variables -->
+//     datatype varName = value;
+
+// Some datatypes are --> 
+//     Primitive -->
+//         Integer --> byte(1), short(2), int(4), long(8)
+//         Floats --> double(8), float(4)
+//             doubles have more precision than floats hence java supports doubles by default.
+//         Character --> char(2), ' Single qoutes used '
+//             chars in java follow UNICODE and not the ASCII and hence it takes 2 bytes of storage.
+//         Boolean --> boolean(1)
+//             java supports only two keywords True / False for bools
+//     
+//      User Defined --> 
+
